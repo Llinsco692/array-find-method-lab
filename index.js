38e2ccac93b85350f9602db984c08cb726220b6b
collins muriuki <muriukicollins48@gmail.com>
2025-03-10 15:56:07 +0300

    added a call back function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(records) {
+    // Use the find() method to search for the first win (where result is "W")
+    const winRecord = records.find(record => record.result === "W");
+    
+    // If a win is found, return the year; otherwise, return undefined
+    return winRecord ? winRecord.year : undefined;
+  }
+  
